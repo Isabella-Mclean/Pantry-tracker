a821cf92d7fcc75719246f993c1ed137fbb4bd90
Isabella <80723693+Isabella-Mclean@users.noreply.github.com>
2024-07-31 11:37:41 +0100

    Fixed error caused by analytics

--- pantry-app/firebase.js
+++ pantry-app/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore";
 require('dotenv').config();
 
@@ -17,11 +16,9 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app)
 export {app, firestore}
