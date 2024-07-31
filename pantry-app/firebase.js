ec9dd41f5b5a231ceef797ed0bbb0dd68eede9f8
Isabella <80723693+Isabella-Mclean@users.noreply.github.com>
2024-07-31 11:14:46 +0100

    Removed unnecessary folder, updated vulnerability

--- pantry-app/firebase.js
+++ pantry-app/firebase.js
@@ -2,20 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore";
+require('dotenv').config();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ0Qt-QPLk-2lIDR9_JUxgeBqWn-FjZdE",
-  authDomain: "pantryapp-9c375.firebaseapp.com",
-  projectId: "pantryapp-9c375",
-  storageBucket: "pantryapp-9c375.appspot.com",
-  messagingSenderId: "805104784021",
-  appId: "1:805104784021:web:9c8e1802745ecb4864819c",
-  measurementId: "G-RZR2Z01S68"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
