dadd6295ce48438a25c00056cdf322b09140fabb
Isabella <80723693+Isabella-Mclean@users.noreply.github.com>
2024-08-10 12:43:57 +0100

    Created ai generated recipe using OpenAI rather than Meta Llama
    Need to add streaming effect so it doesn't take so long to load

--- pantry-app/app/api/generateRecipe/route.js
+++ pantry-app/app/api/generateRecipe/route.js
@@ -1,29 +1,19 @@
-import { exec } from 'child_process';
-import path from 'path';
+import { NextResponse } from "next/server"
+import OpenAI from "openai";
 
 
-export const POST = async (req) => {
-  const { message } = await req.json();
-  const scriptPath = path.join(process.cwd(), 'generateRecipe.py');
-
-  return new Promise((resolve) => {
-    exec(`python3 generateRecipe.py "${message}"`, (error, stdout, stderr) => {
-      if (error) {
-        resolve(new Response(JSON.stringify({ error: error.message }), { status: 500 }));
-      } else if (stderr) {
-        resolve(new Response(JSON.stringify({ error: stderr }), { status: 500 }));
-      } else {
-        try {
-          console.log(JSON.parse(stdout));
-          const output = JSON.parse(stdout); // Parse the JSON output from the Python script
-          resolve(new Response(JSON.stringify(output), { status: 200 }));
-        } catch (e) {
-          console.log('Invalid JSON output from Python script');
-          console.log(e);
-          resolve(new Response(JSON.stringify({ error: 'Invalid JSON output from Python script' }), { status: 500 }));
-        }
-      }
-    });
-  });
-};
+export async function POST(req){
+  const openai = new OpenAI();
+  const data = await req.json()
+  const systemPrompt = `
+    You are a helpful recipe generator, that can generate a recipe based on a provided list of items`
+  const completion = await openai.chat.completions.create({
+    messages:[{role:'system',content:systemPrompt},...data],
+    model:'gpt-4o-mini'
+  })
+  return NextResponse.json(
+    {message: completion.choices[0].message.content},
+  {status:200},
+  )
+}
 
